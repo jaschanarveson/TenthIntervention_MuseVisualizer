befba387530f6a18dca735b813ffcd58d4df4dd3
Jascha Narveson <jnarveson@wesleyan.edu>
2017-03-08 19:22:44 -0500

    modify bezier-lines-2 theme

--- p5-visuals/themes/bezier-lines-2.js
+++ p5-visuals/themes/bezier-lines-2.js
@@ -2,16 +2,16 @@
 This theme tries to use all 4 sensor nodes for each of the five bands.
 */
 
-function bezier_lines_2 () {
-    colorMode(HSB, 255);
+function bezier_lines_2() {
+    colorMode(RGB, 255);
     rectMode(CORNER);
     strokeWeight(1);
 
     background(123);
 
 
-    stroke(255);
-    fill(0);
+    stroke(255, 108, 0, 123);
+    fill(255, 108, 0, 123);
 
     bezier(
         muse.alpha[0] * width,
@@ -23,4 +23,19 @@ function bezier_lines_2 () {
         muse.alpha[3] * width,
         muse.beta[3] * height
     );
+
+    stroke(137, 255, 0, 123);
+    fill(137, 255, 0, 123);
+
+    bezier(
+        muse.theta[0] * width,
+        muse.gamma[0] * height,
+        muse.theta[1] * width,
+        muse.gamma[1] * height,
+        muse.theta[2] * width,
+        muse.gamma[2] * height,
+        muse.theta[3] * width,
+        muse.gamma[3] * height
+    );
+
 }
