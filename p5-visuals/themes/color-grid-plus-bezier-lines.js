3a93e7bbcbaf2bab6cbcd2738765ea9e85fee0af
Jascha Narveson <jnarveson@wesleyan.edu>
2017-03-16 06:56:01 -0400

    more work on color-grid-plus-bezier-lines

--- p5-visuals/themes/color-grid-plus-bezier-lines.js
+++ p5-visuals/themes/color-grid-plus-bezier-lines.js
@@ -4,48 +4,40 @@ function color_grid_plus_bezier_lines() {
     var x_spacing = map(muse.concentration, 0, 1, gridmin, gridmax);
     var y_spacing = map(muse.mellow, 0, 1, gridmin, gridmax);
 
+    strokeWeight(1);
+
     colorMode(RGB, 255);
     background(255);
 
     var maxSquares = (width / gridmin) * (height / gridmin);
 
-    stroke(255, 255, 255, 0);
-    strokeWeight(2);
-
     colorMode(HSB, maxSquares);
     rectMode(CENTER);
 
-    var longestLine = dist(0, 0, width/2, height/2);
+    var longestLine = dist(0, 0, width / 2, height / 2);
 
 
-    push();
-    translate(width / 2, height / 2);
+    //    push();
+    //    translate(width / 2, height / 2);
 
-    var sqH = 0;
-    var sqS = maxSquares;
-    var sqB = maxSquares;
+    var squaresWide = floor(width / x_spacing);
+    var squaredHigh = floor(height / y_spacing);
+    console.log(squaresWide, squaredHigh);
+    var centerX = width / 2;
+    var centerY = height / 2;
 
-    for (var x = 0; x < width / 2 + x_spacing; x += x_spacing) {
-        for (var y = 0; y < height / 2 + y_spacing; y += y_spacing) {
+    for (var x = centerX - (squaresWide * x_spacing); x < width + x_spacing; x += x_spacing) {
+        for (var y = centerY - (squaredHigh * y_spacing); y < height + y_spacing; y += y_spacing) {
 
-            fill(map(dist(x, y, 0, 0), 0, longestLine, 0, maxSquares), maxSquares/2, maxSquares/2, maxSquares/2);
+            stroke(map(dist(x, y, centerX, centerY), 0, longestLine, 0, maxSquares), maxSquares / 2, maxSquares / 2, maxSquares / 2);
+            fill(map(dist(x, y, centerX, centerY), 0, longestLine, 0, maxSquares), maxSquares / 2, maxSquares / 2, maxSquares / 2);
             rect(x, y, x_spacing, y_spacing);
-
-            fill(map(dist(x, -y, 0, 0), 0, longestLine, 0, maxSquares), maxSquares/2, maxSquares/2, maxSquares/2);
-            rect(x, -y, x_spacing, y_spacing);
-
-            fill(map(dist(-x, y, 0, 0), 0, longestLine, 0, maxSquares), maxSquares/2, maxSquares/2, maxSquares/2);
-            rect(-x, y, x_spacing, y_spacing);
-
-            fill(map(dist(-x, -y, 0, 0), 0, longestLine, 0, maxSquares), maxSquares/2, maxSquares/2, maxSquares/2);
-            rect(-x, -y, x_spacing, y_spacing);
         }
     }
 
-    pop();
+    //    pop();
 
     colorMode(HSB, 360);
-    strokeWeight(1);
 
     var bands = ["delta", "theta", "alpha", "beta", "gamma"];
 
@@ -54,7 +46,7 @@ function color_grid_plus_bezier_lines() {
 
         fill(360 * (i / bands.length), 360, 360, 60);
         stroke(360 * (i / bands.length), 360, 360, 60);
-
+        //        fill(0, 0, 0, 100);
         bezier(
             // anchor 1
             0,
