05c0ff07b3908085316f123e350ca99d395738f9
Jascha Narveson <jnarveson@wesleyan.edu>
2017-03-23 18:38:23 -0400

    fixed a CPU issue

--- p5-visuals/themes/grid-plus-bezier-lines.js
+++ p5-visuals/themes/grid-plus-bezier-lines.js
@@ -1,6 +1,7 @@
 function grid_plus_bezier_lines() {
     strokeWeight(1);
-    colorMode(HSB, 360);
+
+    colorMode(RGB, 255);
 
     background(180);
 
@@ -8,25 +9,28 @@ function grid_plus_bezier_lines() {
     var gridmax = min([width / 2, height / 2]);
     var x_spacing = map(muse.concentration, 0, 1, gridmin, gridmax);
     var y_spacing = map(muse.mellow, 0, 1, gridmin, gridmax);
-    var numWide = ceil(width/x_spacing);
-    var numHigh = ceil(height/y_spacing);
+    var numWide = ceil(width / x_spacing);
+    var numHigh = ceil(height / y_spacing);
 
     var centerX = width / 2;
     var centerY = height / 2;
 
-    stroke(190, 0, 190, 123);
-    fill(123, 0, 123, 123);
+    stroke(200);
+    fill(123);
 
-    var startX = centerX - ceil(numWide/2) * x_spacing;
-    var startY = centerY - ceil(numHigh/2) * y_spacing;
+    var startX = centerX - ceil(numWide / 2) * x_spacing;
+    var startY = centerY - ceil(numHigh / 2) * y_spacing;
 
     for (var x = startX; x < width + x_spacing; x += x_spacing) {
-        for (var y = startY; y < height + y_spacing; y += y_spacing) {
-            line(x, 0, x, height);
-            line(0, y, width, y);
-        }
+        line(x, 0, x, height);
+    }
+    for (var y = startY; y < height + y_spacing; y += y_spacing) {
+        line(0, y, width, y);
     }
 
+
+
+    colorMode(HSB, 360);
     // bezier lines
 
     var bands = ["delta", "theta", "alpha", "beta", "gamma"];
